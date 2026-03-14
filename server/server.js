c647893ff86e5dd9e51768ac87e35fc355edd8fa
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-03-14 20:59:14 +0530

    modified:   .gitignore 	modified:   server/.env 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 
-import {serve } from "inngest/express"
+import { serve } from "inngest/express"
 import {inngest, functions} from "./inngest/index.js"
 
 const app= express();
